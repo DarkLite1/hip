5054d5badbf828ec03fbd672ec80738eb0a34601
DarkLite1 <BrechtGijbels@gmail.com>
2020-05-20 14:08:32 +0200

    on page refresh keep the user on his current page and set the accountID

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -1,4 +1,11 @@
 // load msal first so it can handle the redirect to the page
 // after a user has been authenticated with loginRedirect()
 
-import 'src/services/auth/authService'
+// a page refresh also sets the accountID and keeps the user on the page
+
+import { boot } from 'quasar/wrappers'
+import { setAccountID } from './../comp-functions/useAccount'
+
+export default boot(() => {
+  setAccountID()
+})
