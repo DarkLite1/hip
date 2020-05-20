51abcb99cd3f758d26b45e7dc705bb07c82d01a3
DarkLite1 <BrechtGijbels@gmail.com>
2020-05-20 10:29:38 +0200

    changed the loading of the composition api from a boot file to a seperate file and import i in the router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
+import 'src/services/vue/CompositionApi'
 import { route } from 'quasar/wrappers'
 import VueRouter from 'vue-router'
 import routes from './routes'
+
+
+import { useAccount } from 'src/comp-functions/useAccount'
+const { isAuthenticated } = useAccount()
 
 /*
  * If not building with SSR mode, you can
@@ -8,7 +13,7 @@ import routes from './routes'
  */
 
 export default route(function ({ Vue }) {
-  Vue.use(VueRouter)
+Vue.use(VueRouter)
 
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
@@ -19,6 +24,14 @@ export default route(function ({ Vue }) {
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE,
+  })
+
+  Router.beforeEach((to, from, next) => {
+    if (isAuthenticated.value || to.path === '/' || to.path === '/login') {
+      next()
+    } else {
+      next('/login')
+    }
   })
 
   return Router
