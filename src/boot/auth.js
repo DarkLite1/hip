fb2f3e496277c87ea2fce14ab16e4fdb26bfd1fb
DarkLite1 <BrechtGijbels@gmail.com>
2020-05-22 16:31:04 +0200

    removed incorrect comment from auht boot file

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -1,13 +1,7 @@
-// load msal first so it can handle the redirect to the page
-// after a user has been authenticated with loginRedirect()
-
-// a page refresh also sets the accountID and keeps the user on the page
-
 import { boot } from 'quasar/wrappers'
 import { setAccountID } from './../comp-functions/useAccount'
 
-console.log('boot file loaded')
-
 export default boot(() => {
+  // page refresh sets the accountID and keeps the user on the page
   setAccountID()
 })
