ede48bd3c05d2b916974398c4e2555751d4b1df8
DarkLite1 <BrechtGijbels@gmail.com>
2020-05-24 08:44:12 +0200

    renamed useAccount.ts to useAuth

---
+++ src/comp-functions/useAuth.ts
@@ -0,0 +1,66 @@
+import { computed, ref, SetupContext } from '@vue/composition-api'
+import { isInternetExplorer } from 'src/services/utils/utilsService'
+import { Screen } from 'quasar'
+import { auth, allScopes } from 'src/services/auth/authService'
+
+const isLoginPopup = Screen.lt.sm || isInternetExplorer ? false : true
+const accountID = ref('')
+
+export const setAccountID = () => {
+  const account = auth.getAccount()
+
+  if (account) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    accountID.value = account.idTokenClaims.oid!
+  } else {
+    accountID.value = ''
+  }
+}
+
+auth
+  .handleRedirectPromise()
+  .then(setAccountID)
+  .catch((error) => {
+    console.log('login with redirect failed: ', error)
+  })
+
+export const useAccount = (context?: SetupContext) => {
+  const loading = ref(false)
+  const disabled = ref(false)
+
+  const login = async () => {
+    loading.value = true
+    disabled.value = true
+
+    if (isLoginPopup) {
+      try {
+        await auth.loginPopup(allScopes)
+
+        if (context?.root.$router.currentRoute.path === '/login') {
+          context?.root.$router.push('/')
+        }
+      } catch (error) {
+        console.log('login with popup failed: ', error)
+      } finally {
+        setAccountID()
+        disabled.value = false
+        loading.value = false
+      }
+    } else {
+      auth.loginRedirect(allScopes) // page reload
+    }
+  }
+
+  const logout = () => {
+    auth.logout()
+  }
+
+  return {
+    accountID: computed(() => accountID.value),
+    isAuthenticated: computed(() => Boolean(accountID.value)),
+    loading: computed(() => loading.value),
+    disabled: computed(() => disabled.value),
+    login,
+    logout,
+  }
+}
