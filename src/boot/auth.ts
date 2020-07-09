533b60e91428c1ed226e95d952deac4bc7f16806
DarkLite1 <BrechtGijbels@gmail.com>
2020-07-09 09:39:09 +0200

    removed comments

--- src/boot/auth.ts
+++ src/boot/auth.ts
@@ -1,26 +1,3 @@
 import 'src/services/auth/authService'
 // load the msal library first
-
-// // "async" is optional;
-// // more info on params: https://quasar.dev/quasar-cli/cli-documentation/boot-files#Anatomy-of-a-boot-file
-// export default (/* { app, router, Vue ... } */) => {
-//   // console.log('call handleResponse')
-//   // handleResponse
-//   // console.log('call handleResponse done')
-
-//   console.log('boot handleResponse')
-
-//   // const currentAccounts = auth.getAllAccounts()
-//   // console.log('boot handleResponse getAllAccounts ', currentAccounts)
-//   // if (currentAccounts === null) {
-//   //   console.log('boot handleResponse currentAccounts null')
-//   //   return
-//   // } else if (currentAccounts.length > 1) {
-//   //   console.log('boot handleResponse currentAccounts > 1')
-//   //   // Add choose account code here
-//   // } else if (currentAccounts.length === 1) {
-//   //   console.log('boot call setAccount with currentAccounts[0]')
-//   //   setAccount(currentAccounts[0])
-//   // }
-//   // console.log('boot handleResponse done')
-// }
+// required for page refresh to stay on the same page when authenticated
--- src/boot/router.ts
+++ src/boot/router.ts
@@ -3,14 +3,9 @@ import { isAuthenticated } from 'src/store/authStore'
 
 export default boot(({ router }) => {
   router.beforeEach((to, from, next) => {
-    console.log('isAuthenticated ', isAuthenticated.value)
-    console.log('router from', from.path)
-    console.log('router to', to.path)
     if (isAuthenticated.value || to.path === '/' || to.path === '/login') {
-      console.log('router next to', to.path)
       next()
     } else {
-      console.log('router force to /login')
       next('/login')
     }
   })
