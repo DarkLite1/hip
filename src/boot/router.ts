4af917dbc3ea19947c76fed12bb72bc352215f59
DarkLite1 <BrechtGijbels@gmail.com>
2020-08-26 09:36:01 +0200

    fixed navgation when not authenticated only go to the login page

--- src/boot/router.ts
+++ src/boot/router.ts
@@ -3,14 +3,14 @@ import { isAuthenticated } from 'src/store/authStore'
 
 export default boot(({ router }) => {
   router.beforeEach((to, from, next) => {
-    console.log('router isAuthenticated: ', isAuthenticated.value)
+    console.log(`route to '${to.path}' authenticated`, isAuthenticated.value)
 
     if (isAuthenticated.value) {
       if (to.path === '/login') next('/')
       else next()
     } else {
-      if (to.path === '/login' || to.path === '/') next()
-      else next('/login')
+      if (to.path !== '/login') next('/login')
+      else next()
     }
   })
 })
