b1504516b240e8604e95f48b8758a0d189b0c142
DarkLite1 <BrechtGijbels@gmail.com>
2020-08-28 14:52:57 +0200

    fixed graphql query

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,23 +3,23 @@
     <p>Welcome {{ profile.givenName }}</p>
     <q-btn color="primary" label="Test" @click="onClick" />
     <h2>All accounts:</h2>
-    <h2 v-if="error">{{ error }}</h2>
     <h2 v-if="loading">Loading...</h2>
-    <div v-else>
-      Done loading
-
+    <div v-else-if="error">Error: {{ error.message }}</div>
+    <div v-else-if="allAccounts">
       <div v-for="account in allAccounts" :key="account.accountIdentifier">
         {{ account.name }}
+        {{ account.accountIdentifier }}
       </div>
     </div>
+    <div v-else>No accounts found</div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import { profile } from 'src/store/graphStore'
-import { useResult } from '@vue/apollo-composable'
 import { useAllAccountsQuery } from 'src/graphql/generated/operations'
+import { useResult } from '@vue/apollo-composable'
 
 export default defineComponent({
   setup() {
@@ -28,7 +28,7 @@ export default defineComponent({
     }
 
     const { result, loading, error } = useAllAccountsQuery()
-    const allAccounts = useResult(result)
+    const allAccounts = useResult(result, null, (data) => data.accounts)
 
     return { onClick, profile, allAccounts, loading, error }
   },
