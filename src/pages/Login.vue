a64787e9ad960105de4fc4abd15a774a3223c59c
DarkLite1 <BrechtGijbels@gmail.com>
2020-09-21 13:47:37 +0200

    updated packages

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,8 +1,20 @@
-<template>
+<template class="body–dark">
   <q-page padding>
-    <h3>Login</h3>
-    <h5>Hi there stranger. Would you mind telling us who you are?</h5>
-    <p>Click on the login button on the top right corner</p>
+    <div class="fixed-center">
+      <h3 class="q-my-none text-center text-uppercase">Welcome</h3>
+      <!-- <h3 class="q-my-none text-center text-uppercase text-bold">Welcome</h3> -->
+      <h5 class="q-mt-none text-center text-uppercase text-no-wrap">
+        HeidelbergCement IT Portal
+      </h5>
+    </div>
+    <!-- <div class="fixed-center q-mt-none">
+      <h2 class="q-mt-none">Welcome</h2>
+      <h3 class="q-mt-none">HeidelbergCement IT Portal</h3>
+    </div> -->
+    <!-- <h2 class="fixed-center q-mt-none">Welcome</h2>
+    <h3 class="fixed-center q-mt-none">HeidelbergCement IT Portal</h3> -->
+    <!-- <p class="fixed-center text-weight-bolder">Welcome</p> -->
+    <!-- <h5>Hi there stranger. Would you mind telling us who you are?</h5> -->
   </q-page>
 </template>
 
