991d422833074c078ca352784828ac9b70d173c7
DarkLite1 <BrechtGijbels@gmail.com>
2020-11-12 15:08:22 +0100

    upgraded to the latest Apollo

--- src/boot/apollo.ts
+++ src/boot/apollo.ts
@@ -1,17 +1,21 @@
 import { boot } from 'quasar/wrappers'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-import { from } from 'apollo-link'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloClient } from 'apollo-client'
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  from,
+} from '@apollo/client/core'
 import { provide } from '@vue/composition-api'
-import { onError } from 'apollo-link-error'
-import { setContext } from 'apollo-link-context'
+import { onError } from '@apollo/link-error'
+import { setContext } from '@apollo/link-context'
 import { getToken } from 'src/services/auth/authService'
 import { ENVIRONMENT } from 'src/environment'
 
 export default boot(({ app }) => {
-  const httpLink = createHttpLink({ uri: ENVIRONMENT.resources.gatewayApi.uri })
+  const httpLink = new HttpLink({
+    uri: ENVIRONMENT.resources.gatewayApi.uri,
+  })
 
   const authMiddleware = setContext(async (req, { headers }) => {
     const token = await getToken(ENVIRONMENT.resources.gatewayApi.scopes)
