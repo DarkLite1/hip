82e251983db457c999b6ff9e15c30ae07ec85a2e
DarkLite1 <BrechtGijbels@gmail.com>
2020-11-18 11:39:05 +0100

    added application card

--- src/pages/Applications.vue
+++ src/pages/Applications.vue
@@ -1,11 +1,26 @@
 <template>
   <q-page padding>
-    <h1>{{ $t('navigationMenu.applications') }}</h1>
+    <div class="q-pa-md row items-start q-gutter-md cursor-pointer">
+      <q-card class="my-card" clickable @click="GetSapRoster">
+        <q-card-section class="bg-primary text-white">
+          <!-- <q-card-section class="bg-secondary text-white"> -->
+          <div class="text-h6">SAP Truck Roster</div>
+          <div class="text-subtitle2">Get the truck planning</div>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 
-export default defineComponent({})
+export default defineComponent({
+  setup() {
+    const GetSapRoster = () => {
+      console.log('get SAP roster clicked')
+    }
+    return { GetSapRoster }
+  },
+})
 </script>
