9a6715b8f126d14d874edcfe7ca3939cb6b6a4cc
DarkLite1 <BrechtGijbels@gmail.com>
2020-11-24 16:33:34 +0100

    added translation for error messages

--- src/i18n/en-us/index.ts
+++ src/i18n/en-us/index.ts
@@ -37,8 +37,8 @@ export default {
         driverIdUnknown: "I don't know my driver ID",
       },
       error: {
-        truckId: "10 characters are required",
-        driverId: "At least 3 characters are required",
+        truckId: "At least 4 and maximum 12 characters are allowed",
+        driverId: "A number between 9800000000 and 9999999999 (10 characters) is required",
       },
       label: {
         truckId: "Truck ID",
