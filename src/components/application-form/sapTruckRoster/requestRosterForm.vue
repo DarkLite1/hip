7ed86669fb01ddb34078b540beb88806ff821869
DarkLite1 <BrechtGijbels@gmail.com>
2020-12-02 08:42:54 +0100

    added separate component for requestRosterForm

---
+++ src/components/application-form/sapTruckRoster/requestRosterForm.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <q-form @submit="onSubmit" @reset="onReset">
+      <div class="q-gutter-sm" style="max-width: 300px">
+        <template v-if="showTruckId">
+          <p class="text-bold">
+            {{ $t('application.sapTruckRoster.question.truckId') }}
+          </p>
+          <q-input
+            :label="$t('application.sapTruckRoster.label.truckId')"
+            v-model="truckId"
+            outlined
+            clearable
+            clear-icon="close"
+            hide-bottom-space
+            lazy-rules
+            :rules="[
+              (val) =>
+                (val &&
+                  val.length > 3 &&
+                  val.length < 13 &&
+                  /[a-zA-Z]/g.test(val)) ||
+                $t('application.sapTruckRoster.error.truckId'),
+            ]"
+          >
+          </q-input>
+        </template>
+
+        <template v-else>
+          <p class="text-bold">
+            {{ $t('application.sapTruckRoster.question.driverId') }}
+          </p>
+          <q-input
+            :label="$t('application.sapTruckRoster.label.driverId')"
+            v-model="driverId"
+            outlined
+            clearable
+            clear-icon="close"
+            hide-bottom-space
+            lazy-rules
+            :rules="[
+              (val) =>
+                (val && val > 9799999999 && val < 9999999999) ||
+                $t('application.sapTruckRoster.error.driverId'),
+            ]"
+          >
+          </q-input>
+        </template>
+
+        <q-checkbox
+          v-model="showTruckId"
+          :label="$t('application.sapTruckRoster.question.driverIdUnknown')"
+        />
+
+        <div>
+          <q-btn :label="$t('button.submit')" type="submit" color="primary" />
+          <q-btn
+            :label="$t('button.reset')"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const showTruckId = ref(false)
+    const truckId = ref()
+    const driverId = ref()
+
+    const onSubmit = () => {
+      console.log('form submitted')
+    }
+    const onReset = () => {
+      console.log('form reset')
+      truckId.value = null
+      driverId.value = null
+      showTruckId.value = false
+    }
+
+    return { showTruckId, onReset, onSubmit, driverId, truckId }
+  },
+})
+</script>
--- src/components/application-form/sapTruckRoster/sapTruckRosterForm.vue
+++ src/components/application-form/sapTruckRoster/sapTruckRosterForm.vue
@@ -1,100 +1,23 @@
 <template>
   <div>
     <available-dispatch-groups />
-
-    <q-form @submit="onSubmit" @reset="onReset">
-      <div class="q-gutter-sm" style="max-width: 300px">
-        <template v-if="showTruckId">
-          <p class="text-bold">
-            {{ $t('application.sapTruckRoster.question.truckId') }}
-          </p>
-          <q-input
-            :label="$t('application.sapTruckRoster.label.truckId')"
-            v-model="truckId"
-            outlined
-            clearable
-            clear-icon="close"
-            hide-bottom-space
-            lazy-rules
-            :rules="[
-              (val) =>
-                (val &&
-                  val.length > 3 &&
-                  val.length < 13 &&
-                  /[a-zA-Z]/g.test(val)) ||
-                $t('application.sapTruckRoster.error.truckId'),
-            ]"
-          >
-          </q-input>
-        </template>
-
-        <template v-else>
-          <p class="text-bold">
-            {{ $t('application.sapTruckRoster.question.driverId') }}
-          </p>
-          <q-input
-            :label="$t('application.sapTruckRoster.label.driverId')"
-            v-model="driverId"
-            outlined
-            clearable
-            clear-icon="close"
-            hide-bottom-space
-            lazy-rules
-            :rules="[
-              (val) =>
-                (val && val > 9799999999 && val < 9999999999) ||
-                $t('application.sapTruckRoster.error.driverId'),
-            ]"
-          >
-          </q-input>
-        </template>
-
-        <q-checkbox
-          v-model="showTruckId"
-          :label="$t('application.sapTruckRoster.question.driverIdUnknown')"
-        />
-
-        <div>
-          <q-btn :label="$t('button.submit')" type="submit" color="primary" />
-          <q-btn
-            :label="$t('button.reset')"
-            type="reset"
-            color="primary"
-            flat
-            class="q-ml-sm"
-          />
-        </div>
-      </div>
-    </q-form>
+    <hr />
+    <requestRosterForm />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
-  name: 'ApplicationForm',
-  setup() {
-    const showTruckId = ref(false)
-    const truckId = ref()
-    const driverId = ref()
-
-    const onSubmit = () => {
-      console.log('form submitted')
-    }
-    const onReset = () => {
-      console.log('form reset')
-      truckId.value = null
-      driverId.value = null
-      showTruckId.value = false
-    }
-
-    return { showTruckId, onReset, onSubmit, driverId, truckId }
-  },
   components: {
     availableDispatchGroups: () =>
       import(
         'src/components/application-form/sapTruckRoster/availableDispatchGroups.vue'
+      ),
+    requestRosterForm: () =>
+      import(
+        'src/components/application-form/sapTruckRoster/requestRosterForm.vue'
       ),
   },
 })
