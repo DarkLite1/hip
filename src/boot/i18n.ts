96f7a904217b246e25b867e77983fd8e26f2ef33
DarkLite1 <BrechtGijbels@gmail.com>
2020-12-03 11:19:30 +0100

    added vue-i18n-composable

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,6 +1,6 @@
 import { boot } from 'quasar/wrappers'
+import { createI18n } from 'vue-i18n-composable'
 import messages from 'src/i18n'
-import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 declare module 'vue/types/vue' {
@@ -9,37 +9,12 @@ declare module 'vue/types/vue' {
   }
 }
 
-Vue.use(VueI18n)
-
-export const i18n = new VueI18n({
+const i18n = createI18n({
   locale: 'en-us',
   fallbackLocale: 'en-us',
   messages,
 })
 
 export default boot(({ app }) => {
-  // Set i18n instance on app
   app.i18n = i18n
 })
-
-
-// import { boot } from 'quasar/wrappers'
-// import { createI18n } from 'vue-i18n-composable'
-// import messages from 'src/i18n'
-// import VueI18n from 'vue-i18n'
-
-// declare module 'vue/types/vue' {
-//   interface Vue {
-//     i18n: VueI18n
-//   }
-// }
-
-// const i18n = createI18n({
-//   locale: 'en-us',
-//   fallbackLocale: 'en-us',
-//   messages,
-// })
-
-// export default boot(({ app }) => {
-//   app.i18n = i18n
-// })
