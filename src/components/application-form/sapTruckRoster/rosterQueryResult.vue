4357f1409b7d176d3a3860960b6e125220ada32a
DarkLite1 <BrechtGijbels@gmail.com>
2020-12-07 11:05:26 +0100

    added rosterQueryResult

--- src/components/application-form/sapTruckRoster/rosterQueryResult.vue
+++ src/components/application-form/sapTruckRoster/rosterQueryResult.vue
@@ -1,52 +1,73 @@
 <template>
   <div class="q-pb-sm">
+    <p>fromDate: {{ fromDate }}</p>
+    <p>driverId: {{ driverId }}</p>
+    <p>truckId: {{ truckId }}</p>
+
     <q-spinner v-if="loading" color="primary" size="3em" />
-    <!-- <div v-else-if="error">Error: {{ error.message }}</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
     <div v-else-if="apiError">Error: {{ apiError.message }}</div>
 
-    <div v-else-if="dispatchGroups" style="max-width: 500px">
-      <div v-for="group of dispatchGroups" :key="group.date" class="q-pb-md">
-        <p>{{ convertDate(group.date) }}</p>
-        <div class="q-gutter-xs">
-          <q-btn
-            v-for="name in group.dispatchGroup"
-            :key="name"
-            color="secondary"
-            :label="name"
-            rounded
-            padding="xs sm"
-            :ripple="false"
-          />
-        </div>
-      </div> 
-    </div> -->
+    <div v-else-if="RosterQueryResult" style="max-width: 500px">
+      <div
+        v-for="trip of RosterQueryResult"
+        :key="trip.startPlantLoadingDateTime"
+        class="q-pb-md"
+      >
+        <p>startPlantLoadingDateTime: {{ trip.startPlantLoadingDateTime }}</p>
+        <p>driverId: {{ trip.driverId }}</p>
+        <p>truckId: {{ trip.truckId }}</p>
+        <p>plantId: {{ trip.plantId }}</p>
+        <p>plantName: {{ trip.plantName }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import { useResult } from '@vue/apollo-composable'
 import { defineComponent } from '@vue/composition-api'
 import { useSapTruckRosterRosterQuery } from 'src/graphql/generated/operations'
 
 export default defineComponent({
   props: {
-    truckId: String,
-    driverId: String,
+    fromDate: {
+      type: Date,
+      required: false,
+      default: new Date(),
+    },
+    truckId: {
+      type: String,
+      required: false,
+    },
+    driverId: {
+      type: String,
+      required: false,
+    },
   },
   setup(props) {
-    const { result, loading, error } = useSapTruckRosterRosterQuery(
-      () => {
-        return {
-          fromDate: new Date('2020-10-28'),
-          truckId: props.truckId,
-          driverId: props.driverId,
-        }
-      },
+    const { result, error, loading } = useSapTruckRosterRosterQuery(
+      () => ({
+        fromDate: props.fromDate,
+        // truckId: props.truckId,
+        // driverId: props.driverId,
+      }),
       {
         fetchPolicy: 'no-cache',
       }
     )
 
-    return { result, loading, error }
+    const RosterQueryResult = useResult(result, [], (data) => {
+      if (data.roster.__typename === 'RosterArray') return data.roster.data
+    })
+
+    const apiError = useResult(result, null, (data) => {
+      if (data.roster.__typename === 'ApiError') {
+        return data.roster
+      }
+    })
+
+    return { apiError, RosterQueryResult, error, loading }
   },
 })
 </script>
