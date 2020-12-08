61689807eb0bafe0a8c212343fb8b5bf5b67e28f
DarkLite1 <BrechtGijbels@gmail.com>
2020-12-08 08:32:10 +0100

    added groupBy utils function

--- src/services/utils/utilsService.ts
+++ src/services/utils/utilsService.ts
@@ -22,3 +22,20 @@ export const convertToTime = (isoDate: string, locale: string) => {
     minute: '2-digit',
   })
 }
+
+type ObjectKey = string | number | symbol
+
+export const groupBy = <
+  K extends ObjectKey,
+  TItem extends Record<K, ObjectKey>
+>(
+  items: TItem[],
+  key: K
+): Record<ObjectKey, TItem[]> =>
+  items.reduce(
+    (result, item) => ({
+      ...result,
+      [item[key]]: [...(result[item[key]] || []), item],
+    }),
+    {} as Record<ObjectKey, TItem[]>
+  )
