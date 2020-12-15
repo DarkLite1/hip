32a03cb19219686d6d0d2cb826bd51af79f33576
DarkLite1 <BrechtGijbels@gmail.com>
2020-12-15 15:26:56 +0100

    added correct date format for summary table

--- src/components/application-form/sapTruckRoster/rosterQueryResultSearchFilterTable.vue
+++ src/components/application-form/sapTruckRoster/rosterQueryResultSearchFilterTable.vue
@@ -10,17 +10,17 @@
       />
     </div>
     <div class="col-12 col-sm-8 col-md-6 col-lg-5">
-      <div class="row q-col-gutter-x-sm">
+      <div v-if="fromDate" class="row q-col-gutter-x-sm">
         <div class="col-12 col-sm-4">{{ t('general.fromDate') }}:</div>
-        <div class="col-12 col-sm-8">{{ fromDate }}</div>
+        <div class="col-12 col-sm-8">{{ date }}</div>
       </div>
-      <div class="row q-col-gutter-x-sm">
+      <div v-if="driverId" class="row q-col-gutter-x-sm">
         <div class="col-12 col-sm-4">
           {{ t('application.sapTruckRoster.label.driverId') }}:
         </div>
         <div class="col-12 col-sm-8">{{ driverId }}</div>
       </div>
-      <div class="row q-col-gutter-x-sm">
+      <div v-if="truckId" class="row q-col-gutter-x-sm">
         <div class="col-12 col-sm-4">
           {{ t('application.sapTruckRoster.label.truckId') }}:
         </div>
@@ -32,8 +32,8 @@
 
 <script lang="ts">
 import { useI18n } from 'vue-i18n-composable'
-import { defineComponent, ref } from '@vue/composition-api'
-
+import { defineComponent, computed } from '@vue/composition-api'
+import { convertToDate } from 'src/services/utils/utilsService'
 export default defineComponent({
   props: {
     fromDate: {
@@ -49,12 +49,19 @@ export default defineComponent({
       required: false,
     },
   },
-  setup() {
-    const showFormSubmitResult = ref(false)
+  setup(props) {
+    const { locale } = useI18n()
+
+    const date = computed(() => {
+      if (props.fromDate) {
+        return convertToDate(props.fromDate, locale.value)
+      }
+      return 'NA'
+    })
 
     return {
       ...useI18n(),
-      showFormSubmitResult,
+      date,
     }
   },
 })
