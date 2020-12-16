f153572847c9c78b16862ba4bf4b2c0603fbf263
DarkLite1 <BrechtGijbels@gmail.com>
2020-12-16 16:39:05 +0100

    set dark mode true on logout page by default

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,29 +1,22 @@
-<template class="body–dark">
+<template>
   <q-page padding>
     <div class="fixed-center">
-      <h3 class="q-my-none text-center text-uppercase">Welcome</h3>
-      <!-- <h3 class="q-my-none text-center text-uppercase text-bold">Welcome</h3> -->
-      <h5 class="q-mt-none text-center text-uppercase text-no-wrap">
+      <h5 class="q-mt-none text-center text-uppercase">
         HeidelbergCement IT Portal
       </h5>
+      <h5 class="q-tt-none text-center text-subtitle1">Please log in...</h5>
     </div>
-    <!-- <div class="fixed-center q-mt-none">
-      <h2 class="q-mt-none">Welcome</h2>
-      <h3 class="q-mt-none">HeidelbergCement IT Portal</h3>
-    </div> -->
-    <!-- <h2 class="fixed-center q-mt-none">Welcome</h2>
-    <h3 class="fixed-center q-mt-none">HeidelbergCement IT Portal</h3> -->
-    <!-- <p class="fixed-center text-weight-bolder">Welcome</p> -->
-    <!-- <h5>Hi there stranger. Would you mind telling us who you are?</h5> -->
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, watch } from '@vue/composition-api'
+import { Dark } from 'quasar'
 import { isAuthenticated } from 'src/store/authStore'
 
 export default defineComponent({
   setup(props, context) {
+    Dark.set(true)
     watch(isAuthenticated, (isAuthenticated) => {
       if (isAuthenticated) {
         void context.root.$router.push('/')
