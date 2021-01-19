d92cd62ccc52070bab1cc6a1cd47f05822d0ea74
DarkLite1 <BrechtGijbels@gmail.com>
2021-01-19 16:03:42 +0100

    added mdi-v5 icon support

--- quasar.conf.js
+++ quasar.conf.js
@@ -21,7 +21,7 @@ module.exports = configure(function (ctx) {
 
     extras: [
       // 'ionicons-v4',
-      // 'mdi-v5',
+      'mdi-v5',
       // 'fontawesome-v5',
       // 'eva-icons',
       // 'themify',
